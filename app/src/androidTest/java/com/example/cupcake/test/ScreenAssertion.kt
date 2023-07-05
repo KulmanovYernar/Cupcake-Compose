e6fd68f489f7054e8ae4d961cad22c818a8392cc
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-07-05 17:57:54 +0600

    Add navigation tests

---
+++ app/src/androidTest/java/com/example/cupcake/test/ScreenAssertion.kt
@@ -0,0 +1,9 @@
+package com.example.cupcake.test
+
+import androidx.navigation.NavController
+import junit.framework.Assert.assertEquals
+
+
+fun NavController.assertCurrentRouteName(expectedRouteName:String){
+    assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+}
